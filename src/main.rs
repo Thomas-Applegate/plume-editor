8861300e1bae82b01c21114afa2d26876b4e1f38
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-06-29 17:15:03 -0600

    add text box

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use fltk::{app, prelude::*, window::Window};
+use fltk::{app, prelude::*, window::Window, text::TextEditor,
+	text::TextBuffer, enums::Font};
 
 fn main() {
     let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    let mut wind = Window::default().with_label("Plume Editor")
+		.with_size(500, 600)
+		.center_screen();
+	wind.size_range(150,100, 0, 0);
+    wind.make_resizable(true);
+    
+    let buf = TextBuffer::default();
+    
+    let mut txt = TextEditor::default().size_of_parent();
+    txt.set_text_font(Font::Courier);
+    txt.set_buffer(buf);
+    
     wind.end();
     wind.show();
     app.run().unwrap();
